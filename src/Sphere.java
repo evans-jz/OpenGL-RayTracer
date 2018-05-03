558dadb4fe4bb888bb36178aea5399cbbd3eeabf
Evans Zhang <evanszhang95@gmail.com>
2018-05-03 17:56:44 -0400

    Initial set up

---
+++ src/Sphere.java
@@ -0,0 +1,22 @@
+// Sphere class
+// defines a Sphere shape
+
+import javax.vecmath.*;
+
+public class Sphere extends Shape {
+	private Vector3f center;	// center of sphere
+	private float radius;		// radius of sphere
+
+	public Sphere() {
+	}
+	public Sphere(Vector3f pos, float r, Material mat) {
+		center = new Vector3f(pos);
+		radius = r;
+		material = mat;
+	}
+	public HitRecord hit(Ray ray, float tmin, float tmax) {
+
+		/* YOUR WORK HERE: complete the sphere's intersection routine */
+		return null;
+	}
+}
