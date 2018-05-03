51b9ce056ccac9d7690456bb560b63790a4fa4b5
Evans Zhang <evanszhang95@gmail.com>
2018-05-03 19:44:50 -0400

    completed sphere and triangle

--- src/Sphere.java
+++ src/Sphere.java
@@ -14,9 +14,63 @@ public class Sphere extends Shape {
 		radius = r;
 		material = mat;
 	}
-	public HitRecord hit(Ray ray, float tmin, float tmax) {
 
-		/* YOUR WORK HERE: complete the sphere's intersection routine */
-		return null;
-	}
+    public HitRecord hit(Ray ray, float tmin, float tmax) {
+
+        /* YOUR WORK HERE: complete the sphere's intersection routine */
+        Vector3f origin = new Vector3f(ray.getOrigin());
+        Vector3f direction = new Vector3f(ray.getDirection());
+
+        origin.sub(center);
+
+        float a = direction.lengthSquared();
+        float b = 2* origin.dot(direction);
+        float c = origin.lengthSquared() - (radius * radius);
+        float rootValue = (float) (b * b - 4.0 * a * c);
+        
+        float sqRoot = (float) Math.sqrt(rootValue);
+        
+        if (rootValue <= 0) {
+            return null;
+        }
+
+        float temp1 = (-b + sqRoot) / (2.0f * a);
+        float temp2 = (-b - sqRoot) / (2.0f * a);
+
+        float temp;
+
+        if(rootValue == 0){
+            if ( temp1 >= 0 && temp1 >= tmin && temp1 <= tmax ){
+                temp = temp1;
+            }
+            else
+                return null;
+        }
+        else if(rootValue > 0){
+            if (temp1 > tmin && temp1 <= tmax) {
+                if (tmin<=temp2 && temp2<=tmax && temp2 < temp1){
+                    temp=temp2;
+                }else {
+                    temp = temp1;
+                }
+            } else if (tmin<=temp2 && temp2<=tmax) {
+                temp = temp2;
+            } else {
+                return null;
+            }
+        }
+        else
+            return null;
+
+        Vector3f normal = new Vector3f(ray.pointAt(temp));
+        normal.sub(center);
+        HitRecord rec = new HitRecord();
+        rec.pos = ray.pointAt(temp);
+        rec.t = temp;
+        rec.material = material;
+        rec.normal = normal;
+        rec.normal.normalize();
+
+        return rec;
+    }
 }
--- src/Triangle.java
+++ src/Triangle.java
@@ -41,6 +41,52 @@ public class Triangle extends Shape {
 		/* YOUR WORK HERE: complete the triangle's intersection routine
 		 * Normal should be computed by a bilinear interpolation from n0, n1 and n2
 		 * using the barycentric coordinates: alpha, beta, (1.0 - alpha - beta) */
-		return null;
+		Vector3f d = ray.getDirection();
+		Matrix3f original = new Matrix3f(p1.x - p0.x, p2.x - p0.x, -d.x,
+				p1.y - p0.y, p2.y - p0.y, -d.y,
+				p1.z - p0.z, p2.z - p0.z, -d.z);
+		Vector3f answer = new Vector3f(ray.getOrigin().x - p0.x, ray.getOrigin().y - p0.y,
+				ray.getOrigin().z - p0.z);
+		Matrix3f mx = new Matrix3f(p1.x - p0.x, p2.x - p0.x, -d.x,
+				p1.y - p0.y, p2.y - p0.y, -d.y,
+				p1.z - p0.z, p2.z - p0.z, -d.z);
+		mx.setColumn(0, answer);
+		Matrix3f my = new Matrix3f(p1.x - p0.x, p2.x - p0.x, -d.x,
+				p1.y - p0.y, p2.y - p0.y, -d.y,
+				p1.z - p0.z, p2.z - p0.z, -d.z);
+		my.setColumn(1, answer);
+		Matrix3f mz = new Matrix3f(p1.x - p0.x, p2.x - p0.x, -d.x,
+				p1.y - p0.y, p2.y - p0.y, -d.y,
+				p1.z - p0.z, p2.z - p0.z, -d.z);
+		mz.setColumn(2, answer);
+
+		float dOriginal = original.determinant();
+		float beta = mx.determinant() / dOriginal;
+		float gamma = my.determinant() / dOriginal;
+		float alpha = 1 - beta - gamma;
+		float t = mz.determinant() / dOriginal;
+
+		if((alpha >= 0) && (beta >= 0) && (gamma >= 0) && (t >= tmin) && (t <= tmax)){
+			//hit
+			//normal=	alpha*N0 + beta*N1 + gamma*N2
+			Vector3f tempn0 = new Vector3f(n0);
+			Vector3f tempn1 = new Vector3f(n1);
+			Vector3f tempn2 = new Vector3f(n2);
+			tempn0.scale(alpha);//scale
+			tempn1.scale(beta);
+			tempn2.scale(gamma);
+			tempn0.add(tempn1);//add
+			tempn0.add(tempn2);
+
+			HitRecord rec = new HitRecord();
+			rec.pos = ray.pointAt(t);		// position of hit point
+			rec.t = t;						// parameter t (distance along the ray)
+			rec.material = material;		// material
+			rec.normal = new Vector3f(tempn0);			// norm calculated above
+			rec.normal.normalize();			// normal should be normalized
+			return rec;
+		} else {
+			return null;
+		}
 	}
 }
